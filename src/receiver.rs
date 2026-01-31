9bb7ad331f0d738b73a4b1e36633d92df37ccf7e
Adam Stark <adamstark.uk@gmail.com>
2026-01-31 16:18:04 +0000

    OscReceiver: add documentation

--- src/receiver.rs
+++ src/receiver.rs
@@ -3,10 +3,12 @@ use crate::helpers::{self, OscParseError};
 use crate::message::OscMessage;
 use crate::bundle::OscBundle;
 
+/// Struct for receiving OSC messages and bundles over UDP
 pub struct OscReceiver {
   socket: UdpSocket
 }
 
+/// Represents either an OSC Message or an OSC Bundle
 pub enum OscPacket {
   Message(OscMessage),
   Bundle(OscBundle)
@@ -19,6 +21,7 @@ pub enum OscNetworkError {
 
 impl OscReceiver {
 
+  /// Constructs a new OscReceiver listening on the given port
   pub fn new(port: u32) -> Result<Self, OscNetworkError> {
     let addr = format!("0.0.0.0:{}", port);
     let socket = UdpSocket::bind(addr);
@@ -28,6 +31,7 @@ impl OscReceiver {
     Ok (OscReceiver { socket: socket.unwrap() })
   }
 
+  /// Receives OSC Messages or Bundles from the UDP socket
   pub fn get_messages(&self) -> Result<OscPacket, OscParseError> {
 
     let mut buf = [0u8; 2048];
