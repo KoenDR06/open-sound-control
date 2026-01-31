ade8d871f3677b4c3cf651396d0fe83e576ab796
Adam Stark <adamstark.uk@gmail.com>
2026-01-31 15:59:25 +0000

    Update receive example

--- examples/receive_messages.rs
+++ examples/receive_messages.rs
@@ -2,13 +2,14 @@
 use open_sound_control::*;
 
 fn main() {
-  let receiver = OscReceiver::new(9000).unwrap();
+    let listen_port = 9000;
+    let receiver = OscReceiver::new(listen_port).unwrap();
 
-  loop {
-      match receiver.get_messages() {
-          Ok(OscPacket::Message(msg)) => println!("Got message: {:?}", msg.address),
-          Ok(OscPacket::Bundle(bundle)) => println!("Got bundle: {:?}", bundle.time_tag.seconds),
-          Err(err) => eprintln!("Parse error: {:?}", err),
-      }
-  }
+    loop {
+        match receiver.get_messages() {
+            Ok(OscPacket::Message(msg)) => println!("Got message: {}", msg.to_string()),
+            Ok(OscPacket::Bundle(bundle)) => println!("Got bundle: {:?}", bundle.time_tag.seconds),
+            Err(err) => eprintln!("Parse error: {:?}", err),
+        }
+    }
 }
